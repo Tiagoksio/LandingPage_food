9573f16fede7704e297dd987519d5666c335aaf9
Tiago Oliveira <67693562+Tiagoksio@users.noreply.github.com>
2025-02-10 01:51:24 -0300

    feat: interação menu mobile

--- src/script.js
+++ src/script.js
@@ -1,8 +1,12 @@
 class DomController {
     constructor() {
         this.navMenuMobile = document.querySelector(".nav-menu");
+        this.header = document.querySelector("header");
         this.mobile_btn = document.querySelector(".btn-mobile");
         this.mobile_btn.addEventListener("click", () => this.openCloseMobileMenu())
+        this.boxShadowHeader = window.addEventListener("scroll", () => {
+            this.header.style.boxShadow = (window.scrollY > 0 ? "rgba(0, 0, 0, 0.1) 5px 1px 5px" : "none");  
+        })
     }
     openCloseMobileMenu() {
         if (this.navMenuMobile.style.display !== "none") {
