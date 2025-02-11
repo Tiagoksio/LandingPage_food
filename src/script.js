d41ff762fb66a6661839d6c38d3f26e359541097
Tiago Oliveira <67693562+Tiagoksio@users.noreply.github.com>
2025-02-11 03:52:03 -0300

    fix: Falha na exibição do topBarMenu

--- src/script.js
+++ src/script.js
@@ -1,19 +1,18 @@
 class DomController {
     constructor() {
-        this.navMenuMobile = document.querySelector(".nav-menu");
+        this.topBarMenuDisplay = document.querySelector(".nav-menu");
         this.header = document.querySelector("header");
         this.mobile_btn = document.querySelector(".btn-mobile");
-        this.mobile_btn.addEventListener("click", () => this.openCloseMobileMenu())
         this.boxShadowHeader = window.addEventListener("scroll", () => {
             this.header.style.boxShadow = (window.scrollY > 0 ? "rgba(0, 0, 0, 0.1) 5px 1px 5px" : "none");  
         })
+        this.mobile_btn.addEventListener("click", () => this.openCloseMobileMenu())
+        this.displayTopBar = window.addEventListener("resize", () => {
+            this.topBarMenuDisplay.style.display = window.innerWidth > 1170 ? "flex" : "none"
+        })
     }
     openCloseMobileMenu() {
-        if (this.navMenuMobile.style.display !== "none") {
-            this.navMenuMobile.style.display = "none"
-        } else {
-            this.navMenuMobile.style.display = "flex"
-        }
+        this.topBarMenuDisplay.style.display = this.topBarMenuDisplay.style.display === "flex" ? "none" : "flex"
     }
     
 }
